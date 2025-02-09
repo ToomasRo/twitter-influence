9b52c476ff17811c82cbf3481b2a38da44eb5171
ToomasRo <toomas.roosma@gmail.com>
2025-02-09 09:51:11 +0000

    updated api

--- src/api.py
+++ src/api.py
@@ -1,29 +1,22 @@
 from fastapi import FastAPI
 import pandas as pd
 import uvicorn
-from get_5_latest import get_latest_top_posts
+from .get_5_latest import get_latest_top_posts
 
-url = "https://apis.datura.ai/twitter"
-api_key = "dt_$LSO2gvfJtB6UENHrgs-SS1w0zfSKmAr1gfkbBRmTkIg"
-headers = {
-    "Authorization": api_key,
-    "Content-Type": "application/json"
-}
 
-# Example DataFrame
-top_users_df = pd.DataFrame({
-    "author": ["AlemzadehC", "KevinSvenson_", "invest_answers", "Mrcapitalv", "BitPaine"],
-    "reputation": [19.235544663139578, 13.697349627943815, 20.51674997308669, 17.449652892768967, 19.83055281407229],
-    "engagement_score": [233.45714285714286, 168.95428571428573, 263.62857142857143, 54.942852263946754, 176.42857142857142],
-    "accuracy_score": [1.0, 0.8400000000000001, 1.0, 0.8, 1.0],
-    "debate_quality": [5.387730227342655, 1.1637212945016202, 7.542306146680807, 5.0855709854107936, 8.234224175749421]
-})
-
+top_users_df = pd.read_csv("./data/reputation_data.csv")
 app = FastAPI()
 
+
 @app.get("/top_users")
-def get_top_users():
-    return top_users_df.to_dict(orient="records")
+def get_top_users(n: int = 5):
+
+    return (
+        top_users_df.sort_values("reputation", ascending=False)
+        .head(n)
+        .to_dict(orient="records")
+    )
+
 
 @app.get("/latest_posts/{username}")
 def get_latest_posts(username):
@@ -32,6 +25,12 @@ def get_latest_posts(username):
         return tweets
     except Exception as e:
         return {"error": str(e)}
-    
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
--- src/get_5_latest.py
+++ src/get_5_latest.py
@@ -1,14 +1,18 @@
 import requests
 import json
 import datetime
-from twitter_scraper import get_replies
+from .twitter_scraper import get_replies
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 url = "https://apis.datura.ai/twitter"
-api_key = "dt_$LSO2gvfJtB6UENHrgs-SS1w0zfSKmAr1gfkbBRmTkIg"
-headers = {
-    "Authorization": api_key,
-    "Content-Type": "application/json"
-}
+api_key = os.getenv("DATURA_API_KEY")
+
+headers = {"Authorization": api_key, "Content-Type": "application/json"}
+
 
 ### scrape twitter
 ### returns a json containing top 18/19 tweets
@@ -19,29 +23,40 @@ def scrape_twitter(username, start_date, end_date):
         "end_date": end_date,
         "sort": "Top",
         "lang": "en",
-        "is_retweet": False
+        "is_retweet": False,
     }
     response = requests.request("POST", url, json=payload, headers=headers)
     scraped_tweets = json.loads(response.text)
     return scraped_tweets[0:5]
+
 
 ### emits unneeded data in json and adds replies for each tweet
 ### returns json containing list of hot tweets with their top replies
 def filter_data(data, start_date, end_date):
     list_of_tweets = []
     for i in range(len(data)):
-        new_dict = {"text": data[i]["text"], "reply_count": data[i]["reply_count"], "retweet_count": data[i]["retweet_count"], "like_count": data[i]["like_count"], "quote_count": data[i]["quote_count"], "created_at": data[i]["created_at"], "replies": get_replies(data[i]["conversation_id"], start_date, end_date)}
+        new_dict = {
+            "text": data[i]["text"],
+            "reply_count": data[i]["reply_count"],
+            "retweet_count": data[i]["retweet_count"],
+            "like_count": data[i]["like_count"],
+            "quote_count": data[i]["quote_count"],
+            "created_at": data[i]["created_at"],
+            "replies": get_replies(data[i]["conversation_id"], start_date, end_date),
+        }
         list_of_tweets.append(new_dict)
     return list_of_tweets
 
+
 def get_latest_top_posts(username):
     start_date = datetime.date.today() + datetime.timedelta(days=-2)
-    start_date = start_date.strftime('%Y-%m-%d')
-    end_date = datetime.date.today().strftime('%Y-%m-%d')
+    start_date = start_date.strftime("%Y-%m-%d")
+    end_date = datetime.date.today().strftime("%Y-%m-%d")
     scraped_data = scrape_twitter(username, start_date, end_date)
     filtered_scraped_data = filter_data(scraped_data, start_date, end_date)
     return filtered_scraped_data
 
-if __name__ == "__main__":  
+
+if __name__ == "__main__":
     latest_top = get_latest_top_posts("AlemzadehC")
     print(latest_top)
